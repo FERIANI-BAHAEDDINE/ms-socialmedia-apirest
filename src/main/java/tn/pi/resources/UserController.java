9caf0b22c7703568ee4b252c25c4566e63116972
Baha <bahaeddin.feriani@gmail.com>
2025-09-20 14:12:36 +0100

    user controller

--- src/main/java/tn/pi/resources/UserController.java
+++ src/main/java/tn/pi/resources/UserController.java
@@ -1,8 +1,7 @@
 package tn.pi.resources;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 import tn.pi.daos.UserDao;
 import tn.pi.models.User;
 
@@ -10,6 +9,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
+
 public class UserController {
 
     private final UserDao repository;
@@ -22,8 +23,21 @@ public class UserController {
     public List<User> findAll() {
         List<User> users =  repository.findAll();
         if (users.isEmpty()) {
-            throw new RuntimeException("User not found");
+            log.info("Users not found");
         }
+        log.info("Users not found");
+
         return users;
     }
+
+    @GetMapping("/users/{user_id}")
+    public User findOneUser(@PathVariable String user_id) {
+        return repository.findById(Integer.valueOf(user_id)).orElse(null);
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        return repository.save(user);
+    }
+
 }
